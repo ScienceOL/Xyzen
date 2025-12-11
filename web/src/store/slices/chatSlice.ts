8b5fb8ce480c5d2417fd2587c51daab128c308e0
xinquiry <yinfeng2357@outlook.com>
2025-12-11 11:26:13 +0800

    fix: sourcery suggestions

--- web/src/store/slices/chatSlice.ts
+++ web/src/store/slices/chatSlice.ts
@@ -178,10 +178,14 @@ export interface ChatSlice {
   updateSessionConfig: (
     sessionId: string,
 <<<<<<< HEAD
+<<<<<<< HEAD
     config: { provider_id?: string; model?: string },
 =======
     config: { override_provider_id?: string; override_model?: string },
 >>>>>>> 3d4004b (feat: implement better provider)
+=======
+    config: { provider_id?: string; model?: string },
+>>>>>>> 8b68d30 (fix: sourcery suggestions)
   ) => Promise<void>;
   updateSessionProviderAndModel: (
     sessionId: string,
